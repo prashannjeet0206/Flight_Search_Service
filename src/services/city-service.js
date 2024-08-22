830292eca20eaee80f7dde3bc16ae5752080a86e
prashannjeetHexan <prasannjeet.p@hexantech.com>
2024-08-22 16:19:47 +0530

    added city service layer

---
+++ src/services/city-service.js
@@ -0,0 +1,43 @@
+const { CityRepository } = require("../repository/index");
+
+class CityService {
+  constructor() {
+    this.cityRepository = new CityRepository();
+  }
+  async createCity(data) {
+    try {
+      const city = await this.cityRepository.createCity(data);
+      return city;
+    } catch (error) {
+      console.log("Error in the Service layer of create city");
+      throw { error };
+    }
+  }
+  async deleteCity(cityid) {
+    try {
+      const response = await this.cityRepository.deleteCity(cityid);
+      return response;
+    } catch (error) {
+      console.log("Error in the service layer of delete city");
+    }
+  }
+  async updateCity(cityid, data) {
+    try {
+      const city = await this.cityRepository.updateCity(cityid, data);
+      return city;
+    } catch (error) {
+      console.log("Error in the service layer of update");
+      throw { error };
+    }
+  }
+  async getCity(cityid) {
+    try {
+      const city = await this.cityRepository.getCity(cityid);
+      return city;
+    } catch (error) {
+      console.log("Error in the service layer of get city");
+    }
+  }
+}
+
+module.exports = CityService;
