bd2019c95915e17913faa0791ea62a03e3505ca9
prashannjeetHexan <prasannjeet.p@hexantech.com>
2024-08-23 00:04:01 +0530

    get city by id is working

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -6,5 +6,7 @@ const CityController = require("../../controllers/city-controller");
 
 router.post("/city", CityController.createCity);
 router.delete("/city/:id", CityController.deleteCity);
+router.get("/city/:id", CityController.getCity);
+router.patch("/city/:id", CityController.updateCity);
 
 module.exports = router;
