3a492a1b7c85221220cb631e12eb7a9867132306
prashannjeetHexan <prasannjeet.p@hexantech.com>
2024-08-22 23:55:57 +0530

    created post api for city

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const { PORT } = require("./config/server");
 const bodyParser = require("body-parser");
+const ApiRoutes = require("./routes/index");
 
 const setUpAndStartServer = () => {
   const app = express();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", ApiRoutes);
 
   app.listen(PORT, () => {
     console.log(`Listening to Port:${PORT}`);
