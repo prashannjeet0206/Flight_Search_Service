f87e26dfb0c8842a599b710a6a41867f9a36e843
prashannjeetHexan <prasannjeet.p@hexantech.com>
2024-08-22 12:56:19 +0530

    completed city repository

---
+++ src/repository/city-repository.js
@@ -0,0 +1,49 @@
+const { City } = require("../models/index");
+
+class CityRepository {
+  async createCity({ name }) {
+    try {
+      const city = await City.create({ name });
+      return city;
+    } catch (error) {
+      console.log("error in repository layer");
+      throw { error };
+    }
+  }
+
+  async deleteCity(cityid) {
+    try {
+      await City.destroy({
+        where: {
+          id: cityid,
+        },
+      });
+      return "City Deleted";
+    } catch (error) {
+      console.log("error in repository layer");
+      throw { error };
+    }
+  }
+
+  async updateCity(cityid, data) {
+    try {
+      const city = await City.update(data, { where: { id: cityid } });
+      return city;
+    } catch (error) {
+      console.log("Error in the repository layer");
+      throw { error };
+    }
+  }
+
+  async getCity(cityid) {
+    try {
+      const city = await City.findByPk(cityid);
+      return city;
+    } catch (error) {
+      console.log("Error in Repository Layer");
+      throw { error };
+    }
+  }
+}
+
+module.exports = CityRepository;
