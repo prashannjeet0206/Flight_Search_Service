583a1773901d11cefec25a9c0634325bab7c68db
prashannjeetHexan <prasannjeet.p@hexantech.com>
2024-08-23 00:00:58 +0530

    city delete funtion is working

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const CityController = require("../../controllers/city-controller");
 
 router.post("/city", CityController.createCity);
+router.delete("/city/:id", CityController.deleteCity);
 
 module.exports = router;
